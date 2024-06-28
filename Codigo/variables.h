d31a92920ec92167275b1633d7d0b148e4c6067a
mrstevengz <ssequeira@uamv.edu.ni>
2024-06-28 07:06:18 -0600

    Codigo Final

---
+++ Codigo/variables.h
@@ -0,0 +1,19 @@
+#define MAX_REG 100
+
+
+// Definición de la estructura PERSONA
+struct PERSONA {
+    int id;
+    char nombre[50];
+    char apellidos[50];
+    char telefono[50];
+    char cedula[50];
+    char pagos[100];
+    char fecha[50];
+    char numeroCedula[20];
+    char numeroTelefono[20];
+    char numeroCasa[10];
+    char pago[MAX_REG];
+    int cantidad;
+
+};
